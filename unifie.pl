8c19ffd6c80026df01ab408f1f62e889cde76180
Mohammade Dalati <mohammade.dalati7@etu.univ-lorraine.fr>
2017-11-28 23:10:25 +0100

    occur_check

--- unifie.pl
+++ unifie.pl
@@ -23,15 +23,23 @@ regle(X?=T, simplify) :- var(X), atomic(T).
 regle(X?=T, expand) :- compound(T), \+occur_check(X,T).
 regle(X?=T, check) :- X\=T, occur_check(X,T).
 regle(T?=X, orient) :- var(X), nonvar(T).
-regle(S?=T, decompose) :- 
-regle(S?=T, clash) :-
+regle(S?=T, decompose) :- functor(S,F1,A1), functor(T,F2,A2), F1==F2, A1==A2.
+% regle(S?=T, clash) :-
 
 
 % Retourne vrai si la variable V apparaît dans le terme T, faux sinon
-occur_check(V,T) :- 
+occur_check(V,T) :- var(T), contains_var(V,T).
+occur_check(V,T) :- compound(T), contains_term(V,T);
 
 
-% reduit(R,E,P,Q).
+reduit(rename,X?=T,P,Q) :- X=T.
+reduit(simplify,X?=T,P,Q) :- X=T.
+reduit(expand,X?=T,P,Q) :- X=T.
+reduit(check,X?=T,P,Q) :- X=T.
+reduit(orient,T?=X,P,Q) :-
+reduit(decompose,E,P,Q) :-
+reduit(clash,E,P,Q) :-
+
 
 
 % unifie(P).
