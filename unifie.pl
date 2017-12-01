8285b21ba744d6f4df1b57e36965b57aaa72e45e
Mohammade Dalati <mohammade.dalati7@etu.univ-lorraine.fr>
2017-12-01 21:24:12 +0100

    Ecriture du prédicat unifie

--- unifie.pl
+++ unifie.pl
@@ -21,26 +21,31 @@ echo(_).
 regle(X?=T, rename) :- var(X), var(T).
 regle(X?=T, simplify) :- var(X), atomic(T).
 regle(X?=T, expand) :- compound(T), \+occur_check(X,T).
-regle(X?=T, check) :- X\=T, occur_check(X,T).
-regle(T?=X, orient) :- var(X), nonvar(T).
+regle(X?=T, check) :- X\==T, occur_check(X,T).
+regle(T?=X, orient) :- nonvar(T), var(X).
 regle(S?=T, decompose) :- functor(S,F1,A1), functor(T,F2,A2), F1==F2, A1==A2.
-regle(S?=T, clash) :- functor(S,F1,A1), functor(T,F2,A2), F1\=F2, A1\=A2.
+%Problème dans la règle clash
+%regle(S?=T, clash) :- functor(S,F1,A1), functor(T,F2,A2), F1\==F2, A1\==A2.
 
 
 % Retourne vrai si la variable V apparaît dans le terme T, faux sinon
-occur_check(V,T) :- var(T), contains_var(V,T).
-occur_check(V,T) :- compound(T), contains_term(V,T);
-
-
-reduit(rename,X?=T,[X?=T,Queue],Q) :- X=T, Q=Queue.
-reduit(simplify,X?=T,[X?=T,Queue],Q) :- X=T, Q=Queue.
-reduit(expand,X?=T,[X?=T,Queue],Q) :- X=T, Q=Queue.
-reduit(check,X?=T,[X?=T,Queue],Q) :- 
-reduit(orient,T?=X,P,Q) :-
-reduit(decompose,E,P,Q) :-
-reduit(clash,E,P,Q) :-
+occur_check(V,T) :- var(V), var(T), V==T.
+occur_check(V,T) :- \+ subsumes_term(V,T).
 
 
 
-% unifie(P).
+reduit(rename,X?=T,[X?=T|Queue],Q) :- X=T, Q=Queue.
+reduit(simplify,X?=T,[X?=T|Queue],Q) :- X=T, Q=Queue.
+reduit(expand,X?=T,[X?=T|Queue],Q) :- X=T, Q=Queue.
+reduit(orient,T?=X,[T?=X|Queue],Q) :- T=X, Q=Queue.
+%reduit(decompose,E,P,Q) :-
 
+
+unifie([]) :- true.
+unifie([X?=T|Queue]) :- regle(X?=T,rename), reduit(rename,X?=T,[X?=T|Queue],Q), unifie(Q).
+unifie([X?=T|Queue]) :- regle(X?=T,simplify), reduit(simplify,X?=T,[X?=T|Queue],Q), unifie(Q).
+unifie([X?=T|Queue]) :- regle(X?=T,expand), reduit(expand,X?=T,[X?=T|Queue],Q), unifie(Q).
+unifie([X?=T]) :- regle(X?=T,check), fail.
+unifie([X?=T|Queue]) :- regle(X?=T,orient), reduit(orient,X?=T,[X?=T|Queue],Q), unifie(Q).
+%unifie([X?=T|Queue]) :- regle(X?=T,decompose), reduit(decompose,X?=T,[X?=T|Queue],Q), unifie(Q).
+%unifie([X?=T]) :- regle(X?=T,clash), fail.
